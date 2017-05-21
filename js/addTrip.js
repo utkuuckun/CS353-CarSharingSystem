6f06aac28dd3560921e199659b0d419d48a575d6
albocoder <albocoder@gmail.com>
2017-05-22 00:42:47 +0300

    adding trip

--- js/addTrip.js
+++ js/addTrip.js
@@ -1,7 +1,7 @@
+var i=2;
 $(document).ready(function(){
-    var i=2;
     $("#add_row").click(function(){
-        $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail"+i+"' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile"+i+"' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+        $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='price"+i+"' type='text' placeholder='Price'  class='form-control input-md'></td><td><input  name='hour"+i+"' type='text' placeholder='Hour'  class='form-control input-md'></td><td><input  name='minute"+i+"' type='text' placeholder='Minutes'  class='form-control input-md'></td>");
 
         $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
         i++;
@@ -12,5 +12,22 @@ $(document).ready(function(){
             i--;
         }
     });
-
 });
+
+function addtrip() {
+    timeh = $('#starthour').val();
+    timem = $('#startminute').val();
+
+    ch1_name = $('[name=name0]').val();
+    ch1_price = $('[name=price0]').val();
+    ch1_h = $('[name=hour0]').val();
+    ch1_m = $('[name=minute0]').val();
+
+    ch1_name = $('[name=name1]').val();
+    ch1_price = $('[name=price1]').val();
+    ch1_h = $('[name=hour1]').val();
+    ch1_m = $('[name=minute1]').val();
+
+
+}
+
